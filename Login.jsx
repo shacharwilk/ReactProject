a56b936667a6c0431e1c232d0ed4359c2a83443e
shacharwilk <155710980+shacharwilk@users.noreply.github.com>
2026-03-17 12:56:31 +0200

    Update Login.jsx

--- Login.jsx
+++ Login.jsx
@@ -1,1 +1,13 @@
-<h1> Login </h1>
+let username = "";
+let password = "";
+
+function Login() {
+    return (
+        <div className="login">
+            <h1>Login</h1>
+            <input type="text" placeholder="Username" onChange={(e) => username = e.target.value} />
+            <input type="password" placeholder="Password" onChange={(e) => password = e.target.value} />
+            <button onClick={() => console.log(`Username: ${username}, Password: ${password}`)}>Login</button>
+        </div>
+    );
+}
